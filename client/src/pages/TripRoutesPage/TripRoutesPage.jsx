641bb9a8924940c8a2b57443b26a8f4a7a1dfa6f
andredoc <andre.documet@gmail.com>
2022-04-17 21:12:53 +0200

    add component RouteAccordion

--- client/src/pages/TripRoutesPage/TripRoutesPage.jsx
+++ client/src/pages/TripRoutesPage/TripRoutesPage.jsx
@@ -1,29 +1,11 @@
 import "./TripRoutesPage.css"
-import { Accordion, ListGroup } from "react-bootstrap"
+import RouteAccordion from "../../components/RouteAccordion/RouteAccordion.jsx"
 
-const TripRoutesPage = () => {
+const TripRoutesPage = () => {      
     return (
         <>
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>Detalle de la Ruta</Accordion.Header>
-                    <Accordion.Body>
-                        <ListGroup as="ul">
-                            <ListGroup.Item as="li" >
-                                Unos 9,000 Kms y 26 días, en Mayo 2022, sin paradas prefijadas.
-                            </ListGroup.Item>
-                            <ListGroup.Item as="li">
-                                Desde Madrid, pasando por Barcelona, Francia, Italia, Eslovenia,
-                                Croacia Montenegro, Albania, Grecia, Turquía.
-                            </ListGroup.Item>
-                            <ListGroup.Item as="li" >
-                                La vuelta por Bulgaria, Rumanía, Austria, Suiza, Francia y España.
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>     
-        </>       
+            <RouteAccordion />
+        </>
     )
 }
 
